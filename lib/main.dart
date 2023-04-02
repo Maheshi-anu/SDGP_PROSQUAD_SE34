7be7baff94a9f082e4d06483609ccb2addf37894
Dulsara Denegama <dulsara823@gmail.com>
2023-04-02 18:43:09 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'get_started.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,76 +16,6 @@ class MyApp extends StatelessWidget {
         ),
         body: const MyStatefulWidget(),
         backgroundColor: Colors.white,
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              'Hi There, Welcome to',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                'Beam Load Analyzer',
-                style: TextStyle(
-                    color: Colors.indigo,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 50),
-                textAlign: TextAlign.center,
-              )),
-          Container(
-            child: Image.asset('assets/images/trial1.jpg'),
-            height: 190,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 40,
-            padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.indigo,
-              ),
-              child: const Text('Get Started'),
-              onPressed: () {},
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              const Text('Alreaady have an account?'),
-              TextButton(
-                child: const Text('Sign in',
-                    style: TextStyle(color: Colors.indigo, fontSize: 15)),
-                onPressed: () {
-                  //signup screen
-                },
-              )
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ],
       ),
     );
   }
